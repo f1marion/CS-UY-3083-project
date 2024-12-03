8029eb63928048baab879741e904e496a13d7de3
f1marion <153873595+f1marion@users.noreply.github.com>
2024-12-03 14:51:12 -0500

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,1 +1,10 @@
+# db_connection.py
+import mysql.connector
 
+def get_db_connection():
+    return mysql.connector.connect(
+        host='localhost',
+        user='your_db_username', #replace
+        password='your_db_password', #replace
+        database='Project1_1'
+    )
