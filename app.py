9309ba1119ad7f714ea4a091457af257c8ec2dcb
f1marion <153873595+f1marion@users.noreply.github.com>
2024-12-03 14:55:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,55 @@ def register_customer():
         return redirect(url_for('login'))
     return render_template('register_customer.html')
 
+@app.route('/register_staff', methods=['GET', 'POST'])
+def register_staff():
+    if request.method == 'POST':
+        # Collect form data
+        username = request.form['username']
+        password = request.form['password']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        airline_name = request.form['airline_name']
+        # Hash password
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        # Store in database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO Airline_Staff (Username, Password, Fname, Lname, Airline_name)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (username, hashed_password.decode('utf-8'), fname, lname, airline_name))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('login'))
+    return render_template('register_staff.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_type = request.form['user_type']  # 'customer' or 'staff'
+        username = request.form['username']
+        password = request.form['password'].encode('utf-8')
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        if user_type == 'customer':
+            cursor.execute("SELECT * FROM Customer WHERE Email = %s", (username,))
+        else:
+            cursor.execute("SELECT * FROM Airline_Staff WHERE Username = %s", (username,))
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if user and bcrypt.checkpw(password, user['Password'].encode('utf-8')):
+            session['username'] = username
+            session['user_type'] = user_type
+            if user_type == 'customer':
+                return redirect(url_for('customer_home'))
+            else:
+                return redirect(url_for('staff_home'))
+        else:
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+
